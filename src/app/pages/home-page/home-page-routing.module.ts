6d7c565ca78faab29c11830e8c659479c8ff2aaa
Gadzeman <ostap778800@gmail.com>
2023-01-18 17:36:50 +0200

    minor changes

--- src/app/pages/home-page/home-page-routing.module.ts
+++ src/app/pages/home-page/home-page-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
-import {HomePageComponent} from "./home-page.component";
 import {RouterModule, Routes} from "@angular/router";
+import {HomePageComponent} from "./home-page.component";
 
 const routes: Routes = [
   {
