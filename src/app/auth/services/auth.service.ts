4dc0b5fb8a5a958e2b3e5c6547b7e607eb2fadcf
Gadzeman <ostap778800@gmail.com>
2023-01-20 16:24:08 +0200

    handle access token; refresh token endpoint

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {User} from "../../users/types/users.types";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {AuthSignInBody} from "../types/auth.types";
+import {AuthSignInBody, RefreshTokenBody} from "../types/auth.types";
 import {JwtHelperService} from "@auth0/angular-jwt";
 import {Router} from "@angular/router";
 
@@ -19,6 +19,7 @@ export class AuthService {
   private BASE_URL = 'http://localhost:3000/';
   private signUpEndpoint = 'auth/sign-up';
   private signInEndpoint = 'auth/sign-in';
+  private refreshTokenEndpoint = 'auth/refresh-token';
 
   signUp(body: User): Observable<User> {
     return this.httpClient.post<User>(`${this.BASE_URL}${this.signUpEndpoint}`, body);
@@ -28,11 +29,32 @@ export class AuthService {
     return this.httpClient.post<any>(`${this.BASE_URL}${this.signInEndpoint}`, body);
   }
 
+  refreshToken(body: RefreshTokenBody) {
+    return this.httpClient.put<any>(`${this.BASE_URL}${this.refreshTokenEndpoint}`, body);
+  }
+
   public isSignedUp(): boolean {
     try {
       const token = localStorage.getItem('token');
+      console.log('token: ', token);
 
-      return !this.jwtHelperService.isTokenExpired(token);
+      if (this.jwtHelperService.isTokenExpired(token)) {
+        const { userId } = this.jwtHelperService.decodeToken(token);
+
+        this.refreshToken({ userId }).subscribe({
+          next: (result) => {
+            localStorage.removeItem('token');
+            localStorage.setItem('token', result.accessToken);
+            return true;
+          },
+          error: (error) => {
+            localStorage.removeItem('token');
+            this.router.navigate(['auth/sign-in']);
+            return false;
+          }
+        });
+      }
+      return true;
     } catch (e) {
       this.router.navigate(['auth/sign-in']);
 
