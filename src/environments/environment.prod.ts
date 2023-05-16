fa6cd7a3bd8da3e40ec889cf75fafaf173bf7e90
Gadzeman <ostap778800@gmail.com>
2023-05-16 13:57:18 +0300

    prod env changed on ec2 instance elastic api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  api: 'http://16.16.77.107:3000/',
-  ws: 'ws://16.16.77.107:3001',
+  api: 'http://13.53.97.68:3000/',
+  ws: 'ws://13.53.97.68:3001',
 
   production: true,
 }
