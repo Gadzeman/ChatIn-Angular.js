99712f2358ea9faa7e8b080ac2285bab94d2afe6
Gadzeman <ostap778800@gmail.com>
2023-05-30 16:50:21 +0300

    api changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  api: 'http://10.0.2.69:3000/',
-  ws: 'ws://10.0.2.69:3001',
+  api: 'http://13.50.228.84:3000/',
+  ws: 'ws://13.50.228.84:3001',
 
   production: true,
 }
