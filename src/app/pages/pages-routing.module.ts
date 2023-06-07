404adaa2f20800c8306f22f756897d8ac28c1665
Gadzeman <ostap778800@gmail.com>
2023-06-07 14:55:18 +0300

    auth guard deleted

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from "@angular/router";
 import {NgModule} from "@angular/core";
 import {BasePageComponent} from "./base/base-page.component";
-import {AuthGuard} from "../shared/guards/auth/auth.guard";
 
 const routes: Routes = [
   {
@@ -10,22 +9,18 @@ const routes: Routes = [
     children: [
       {
         path: 'home',
-        canActivate: [AuthGuard],
         loadChildren: () => import('./home/home-page.module').then(m => m.HomePageModule),
       },
       {
         path: 'chats',
-        canActivate: [AuthGuard],
         loadChildren: () => import('./chats/chats-page.module').then(m => m.ChatsPageModule),
       },
       {
         path: 'settings',
-        canActivate: [AuthGuard],
         loadChildren: () => import('./settings/settings-page.module').then(m => m.SettingsPageModule),
       },
       {
         path: 'users',
-        canActivate: [AuthGuard],
         loadChildren: () => import('./users/users-page.module').then(m => m.UsersPageModule),
       },
       {
