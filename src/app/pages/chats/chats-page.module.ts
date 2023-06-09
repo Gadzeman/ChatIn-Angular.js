981363b19ae0537a2db2cabf7d2c89e12eaa52f4
Gadzeman <ostap778800@gmail.com>
2023-06-09 14:46:57 +0300

    working on chat list functionality

--- src/app/pages/chats/chats-page.module.ts
+++ src/app/pages/chats/chats-page.module.ts
@@ -1,13 +1,11 @@
-import {NgModule} from "@angular/core";
-import {ChatsPageComponent} from "./chats-page.component";
-import {ChatsPageRoutingModule} from "./chats-page-routing.module";
+import { NgModule } from '@angular/core';
+import { ChatsPageComponent } from './chats-page.component';
+import { ChatsPageRoutingModule } from './chats-page-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    ChatsPageComponent
-  ],
-  imports: [
-    ChatsPageRoutingModule
-  ]
+  declarations: [ChatsPageComponent],
+  imports: [ChatsPageRoutingModule, MatButtonModule, MatCardModule],
 })
 export class ChatsPageModule {}
