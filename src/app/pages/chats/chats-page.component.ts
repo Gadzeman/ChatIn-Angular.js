6328b706a37a47a10b4bbf8270a026c4f0d2c39f
Gadzeman <ostap778800@gmail.com>
2023-06-09 14:22:19 +0300

    display list of chats with transition

--- src/app/pages/chats/chats-page.component.ts
+++ src/app/pages/chats/chats-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats-page.component.html',
-  styleUrls: ['./chats-page.component.css']
+  styleUrls: ['./chats-page.component.scss'],
 })
-export class ChatsPageComponent {
+export class ChatsPageComponent implements OnInit {
+  public chatsListActive: boolean = false;
 
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  private initData() {
+    this.displayChatsList();
+  }
+
+  private displayChatsList() {
+    setTimeout(() => {
+      this.chatsListActive = true;
+    });
+  }
 }
