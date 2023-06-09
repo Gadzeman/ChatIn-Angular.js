1e4c931e58a0d5697fefbe6f3f24db1d79c519d5
Gadzeman <ostap778800@gmail.com>
2023-06-09 16:02:54 +0300

    add icon and search styles added

--- src/app/pages/chats/chats-page.module.ts
+++ src/app/pages/chats/chats-page.module.ts
@@ -3,9 +3,19 @@ import { ChatsPageComponent } from './chats-page.component';
 import { ChatsPageRoutingModule } from './chats-page-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [ChatsPageComponent],
-  imports: [ChatsPageRoutingModule, MatButtonModule, MatCardModule],
+  imports: [
+    ChatsPageRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatTooltipModule,
+  ],
 })
 export class ChatsPageModule {}
